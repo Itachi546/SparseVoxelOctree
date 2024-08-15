3d66bee2ed7415dddd5523db66ebeb3e4f425f8d
Sujeet Silwal <silwalsujeet149@gmail.com>
2024-08-15 22:37:29 +0545

    Add box intersection before raycasting

--- shaders/raycast-grid.frag.glsl
+++ shaders/raycast-grid.frag.glsl
@@ -37,44 +37,67 @@ float SampleVoxel(vec3 p) {
 #endif
 }
 
+vec2 BoxIntersection(in vec3 ro, in vec3 rd, vec3 boxSize) {
+    vec3 m = 1.0 / rd;
+    vec3 n = m * ro;
+    vec3 k = abs(m) * boxSize;
+    vec3 t1 = -n - k;
+    vec3 t2 = -n + k;
+    float tN = max(max(t1.x, t1.y), t1.z);
+    float tF = min(min(t2.x, t2.y), t2.z);
+    if (tN > tF || tF < 0.0)
+        return vec2(-1.0);
+    return vec2(tN, tF);
+}
+
 const int MAX_ITERATION = 512;
 void main() {
     vec3 r0 = uCamPos.xyz;
     vec3 rd = GenerateCameraRay(uv, uInvP, uInvV);
 
-    vec3 stepDir = sign(rd);
-    vec3 p = floor(r0);
-    vec3 tStep = 1.0f / rd;
-    vec3 offset = 0.5 + stepDir * 0.5;
-    vec3 nextPlane = p + offset;
-    vec3 t = (nextPlane - r0) * tStep;
-
+    float halfSize = VOXEL_GRID_SIZE * 0.5f;
+    vec2 tBox = BoxIntersection(r0, rd, vec3(halfSize));
     vec3 col = vec3(0.5f);
-    for (int i = 0; i < MAX_ITERATION; ++i) {
-        vec3 nearestAxis = step(t, t.yzx) * step(t, t.zxy);
-        p += nearestAxis * stepDir;
-        t += nearestAxis * tStep * stepDir;
+    tBox.x = max(tBox.x, 0.0f);
+    if (tBox.y >= 0.0f) {
+        r0 = r0 + tBox.x * rd;
+        vec3 stepDir = sign(rd);
+        vec3 p = floor(r0);
+        vec3 tStep = 1.0f / rd;
+        vec3 offset = 0.5 + stepDir * 0.5;
+        vec3 nextPlane = p + offset;
+        vec3 t = (nextPlane - r0) * tStep;
 
-        float val = SampleVoxel(p);
-        if (val > 0.1f) {
-            vec3 id = p;
-            p = p + 1.0 - offset;
-            vec3 intersection = (p - r0) * tStep;
-            float d = max(intersection.x, max(intersection.y, intersection.z));
-            p = r0 + d * rd;
-            vec3 n = (p - id - 0.5) * 2.0;
-            n = pow(abs(n), vec3(5.0f)) * sign(n);
-            n = normalize(n);
-            col = n * 0.5 + 0.5;
-            break;
+        float tMax = tBox.y - tBox.x;
+        for (int i = 0; i < MAX_ITERATION; ++i) {
+            vec3 nearestAxis = step(t, t.yzx) * step(t, t.zxy);
+            p += nearestAxis * stepDir;
+            t += nearestAxis * tStep * stepDir;
+
+            float val = SampleVoxel(p);
+            if (val > 0.1f) {
+                vec3 id = p;
+                p = p + 1.0 - offset;
+                vec3 intersection = (p - r0) * tStep;
+                float d = max(intersection.x, max(intersection.y, intersection.z));
+                p = r0 + d * rd;
+                vec3 n = (p - id - 0.5) * 2.0;
+                n = pow(abs(n), vec3(5.0f)) * sign(n);
+                n = normalize(n);
+                col = n * 0.5 + 0.5;
+                // col = vec3(float(i) / float(MAX_ITERATION));
+                break;
+            }
+
+            if (min(t.x, min(t.y, t.z)) > tMax)
+                break;
+            /*
+            if (p.x < -halfSize || p.y < -halfSize || p.z < -halfSize)
+                break;
+            if (p.x > halfSize || p.y > halfSize || p.z > halfSize)
+                break;
+            */
         }
-
-        /*
-         if (p.x < 0 || p.y < 0 || p.z < 0)
-             break;
-         if (p.x > VOXEL_GRID_SIZE || p.y > VOXEL_GRID_SIZE || p.z > VOXEL_GRID_SIZE)
-             break;
-             */
     }
 
     fragColor = vec4(col, 1.0f);
