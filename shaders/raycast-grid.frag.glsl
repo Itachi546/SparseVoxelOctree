7f4b8de7580fe67df6aea199be06cd04c94f5541
Sujeet Silwal <silwalsujeet149@gmail.com>
2024-08-15 23:01:16 +0545

    Jittered shadow sampling

--- shaders/raycast-grid.frag.glsl
+++ shaders/raycast-grid.frag.glsl
@@ -37,6 +37,23 @@ float SampleVoxel(vec3 p) {
 #endif
 }
 
+float CalculateAO(vec3 p) {
+    vec3 ip = round(p);
+    float ao = 0.0f;
+    float totalSample = 0.0f;
+    for (int i = -1; i <= 1; ++i) {
+        for (int j = -1; j <= 1; ++j) {
+            for (int k = -1; k <= 1; ++k) {
+                if (i == 0 && j == 0 && k == 0)
+                    continue;
+                ao += SampleVoxel(ip + vec3(i, j, k));
+                totalSample++;
+            }
+        }
+    }
+    return ao / totalSample;
+}
+
 vec2 BoxIntersection(in vec3 ro, in vec3 rd, vec3 boxSize) {
     vec3 m = 1.0 / rd;
     vec3 n = m * ro;
@@ -50,14 +67,21 @@ vec2 BoxIntersection(in vec3 ro, in vec3 rd, vec3 boxSize) {
     return vec2(tN, tF);
 }
 
-const int MAX_ITERATION = 512;
-void main() {
-    vec3 r0 = uCamPos.xyz;
-    vec3 rd = GenerateCameraRay(uv, uInvP, uInvV);
+vec2 rand22(vec2 p) {
+    p = vec2(dot(p, vec2(127.1, 843.43)),
+             dot(p, vec2(269.5, 183.3)));
 
+    return fract(sin(p) * 43758.5453123);
+}
+
+const vec3 lp = vec3(10.0f, 300.0f, 150.0f);
+const float lr = 10.0f;
+
+const int MAX_ITERATION = 512;
+bool Raycast(vec3 r0, vec3 rd, out vec3 out_p, out vec3 n, out vec3 col) {
     float halfSize = VOXEL_GRID_SIZE * 0.5f;
     vec2 tBox = BoxIntersection(r0, rd, vec3(halfSize));
-    vec3 col = vec3(0.5f);
+    col = vec3(0.5f);
     tBox.x = max(tBox.x, 0.0f);
     if (tBox.y >= 0.0f) {
         r0 = r0 + tBox.x * rd;
@@ -81,12 +105,19 @@ void main() {
                 vec3 intersection = (p - r0) * tStep;
                 float d = max(intersection.x, max(intersection.y, intersection.z));
                 p = r0 + d * rd;
-                vec3 n = (p - id - 0.5) * 2.0;
-                n = pow(abs(n), vec3(5.0f)) * sign(n);
+                out_p = p;
+                n = (p - id - 0.5) * 2.0;
+                vec3 n = pow(abs(n), vec3(5.0f)) * sign(n);
                 n = normalize(n);
-                col = n * 0.5 + 0.5;
+                vec3 ld = normalize(lp - p);
+                col = max(dot(n, ld), 0.1f) * vec3(1.0f, 0.01f, 0.01f) * 5.;
+
+                vec3 h = normalize(-rd + ld);
+                vec3 spec = pow(max(dot(n, h), 0.0f), 16.0f) * vec3(1.);
+                col += spec;
+
                 // col = vec3(float(i) / float(MAX_ITERATION));
-                break;
+                return true;
             }
 
             if (min(t.x, min(t.y, t.z)) > tMax)
@@ -99,6 +130,24 @@ void main() {
             */
         }
     }
+    return false;
+}
 
+void main() {
+    vec3 r0 = uCamPos.xyz;
+    vec3 rd = GenerateCameraRay(uv, uInvP, uInvV);
+    vec3 col, p, _unused, _unused1, n, _unused2;
+    if (Raycast(r0, rd, p, n, col)) {
+        vec3 lpR = lp;
+        // lpR.xz += rand22(p.xz) * lr;
+        float ao = 1 - CalculateAO(p);
+        vec3 ld = normalize(lpR - p);
+        if (Raycast(p + 0.01 * n, ld, _unused, _unused1, _unused2))
+            col *= 0.3f;
+        col *= pow(ao, 3.0f);
+    }
+
+    col /= (1.0f + col);
+    col = pow(col, vec3(0.4545));
     fragColor = vec4(col, 1.0f);
 }
